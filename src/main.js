b56cfc2d6e05772875434cdef549c709d229e1f7
eduardolimacrr <eduardolimacrr@gmail.com>
2025-02-12 17:53:06 -0300

    Project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'  // Importa o router
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)  // Registra o Vue Router na aplicação
+  .mount('#app')  // Monta a aplicação no elemento com o id 'app'
